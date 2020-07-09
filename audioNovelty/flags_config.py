e00151210591919038dd6248ccf3e83c396b39b2
umurotti <umur.gogebakan@gmail.com>
2020-07-09 11:34:12 +0300

    changed max iteration from 1e9 to 1e1

--- audioNovelty/flags_config.py
+++ audioNovelty/flags_config.py
@@ -65,7 +65,7 @@ tf.app.flags.DEFINE_boolean("normalize_by_seq_len", True,
                             "per sequence.")
 tf.app.flags.DEFINE_float("learning_rate", 3e-5,
                           "The learning rate for ADAM.")
-tf.app.flags.DEFINE_integer("max_steps", int(1e9),
+tf.app.flags.DEFINE_integer("max_steps", int(1e1),
                             "The number of gradient update steps to train for.")
 tf.app.flags.DEFINE_integer("summarize_every", 100,
                             "The number of steps between summaries.")
